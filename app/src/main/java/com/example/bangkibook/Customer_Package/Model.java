2e7188a667b3748429fbe3cdf8d12bcacee7ddf5
dechennamgyal <02200144.cst@rub.edu.bt>
2022-09-21 22:06:04 +0600

    customer layout in recycleview

---
+++ app/src/main/java/com/example/bangkibook/Customer_Package/Model.java
@@ -0,0 +1,23 @@
+package com.example.bangkibook.Customer_Package;
+
+public class Model
+{
+    private String header;
+    private String desc;
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
