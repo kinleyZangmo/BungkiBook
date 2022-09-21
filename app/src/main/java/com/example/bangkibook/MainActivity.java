91e21755f7070452ec54105af13b9161b0fa2732
klods <02200150.cst@rub.edu.bt>
2022-09-21 22:50:50 +0600

    genius and klods

--- app/src/main/java/com/example/bangkibook/MainActivity.java
+++ app/src/main/java/com/example/bangkibook/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.bangkibook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginClick(View view) {
+        // Intent intentLogin = new Intent(this, Login.class)
+        // will be added with dechens code
+    }
+
+    // To
+    public void signupClick(View view) {
+
+        Intent intentSignup = new Intent(this, signup.class);
+        startActivity(intentSignup);
+        // intent takes to signup activity
     }
 }
