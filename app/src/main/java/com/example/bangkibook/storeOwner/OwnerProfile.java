65f96bd0ffb643e4137e4dcfc9c20bfdc0e78491
kinleyZangmo <02200154.cst@rub.edu.bt>
2022-10-13 16:17:41 +0600

    Store Owner Profile and log out

--- app/src/main/java/com/example/bangkibook/storeOwner/OwnerProfile.java
+++ app/src/main/java/com/example/bangkibook/storeOwner/OwnerProfile.java
@@ -2,15 +2,33 @@ package com.example.bangkibook.storeOwner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bangkibook.R;
 
 public class OwnerProfile extends AppCompatActivity {
-
+    TextView name,email,phoneNo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_profile);
+
+        name=findViewById(R.id.storeName);
+        email=findViewById(R.id.storeEmail);
+        phoneNo=findViewById(R.id.storePhoneNo);
+
+
+    }
+
+    //Logout Session
+    public void logout(View view) {
+
+        Toast.makeText(this, "Logged Out", Toast.LENGTH_LONG).show();
+        Intent logout = new Intent(this,OwnerMainActivity.class);
+        startActivity(logout);
     }
 }
